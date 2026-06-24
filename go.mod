b01ff92fc1ccbbeefda01b15c275d110a6d6f28d
chrollo-lucifer-12 <sahilpython@gmail.com>
2026-06-24 17:32:23 +0000

    add: saving state of container and adding bundles

--- go.mod
+++ go.mod
@@ -2,8 +2,10 @@ module github.com/container-runtime
 
 go 1.22.2
 
+require github.com/spf13/cobra v1.8.1
+
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/cobra v1.8.1 // indirect
+	github.com/opencontainers/runtime-spec v1.3.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 )
--- operations/create.go
+++ operations/create.go
@@ -1,6 +1,14 @@
 package operations
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/container-runtime/container"
+	"github.com/opencontainers/runtime-spec/specs-go"
+)
 
 type CreateOpts struct {
 	ID     string
@@ -8,7 +16,33 @@ type CreateOpts struct {
 }
 
 func Create(opts *CreateOpts) error {
-	fmt.Println(opts)
+	bundle, err := filepath.Abs(opts.Bundle)
+	if err != nil {
+		return fmt.Errorf("absolute path from bundle: %w", err)
+	}
+
+	config, err := os.ReadFile(filepath.Join(bundle, "config.json"))
+	if err != nil {
+		return fmt.Errorf("read config file: %w", err)
+	}
+
+	var spec *specs.Spec
+	if err := json.Unmarshal(config, &spec); err != nil {
+		return fmt.Errorf("unmarshall config: %w", err)
+	}
+
+	cntr, err := container.New(&container.NewContainerOpts{
+		ID:     opts.ID,
+		Bundle: bundle,
+		Spec:   spec,
+	})
+	if err != nil {
+		return fmt.Errorf("create container: %w", err)
+	}
+
+	if err := cntr.Save(); err != nil {
+		return fmt.Errorf("save container: %w", err)
+	}
 
 	return nil
 }
