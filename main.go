b52c652af7fd73ea53ff83f8f6c10922679c204e
chrollo-lucifer-12 <sahilpython@gmail.com>
2026-06-28 06:25:15 +0000

    add: loopback interface

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 	"syscall"
@@ -23,14 +24,41 @@ func run() {
 	cmd.Stderr = os.Stderr
 
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags:   syscall.CLONE_NEWPID | syscall.CLONE_NEWUTS | syscall.CLONE_NEWNS,
+		Cloneflags:   syscall.CLONE_NEWPID | syscall.CLONE_NEWUTS | syscall.CLONE_NEWNS | syscall.CLONE_NEWNET,
 		Unshareflags: syscall.CLONE_NEWNS,
 	}
 
 	must(cmd.Run())
 }
 
+func cg() {
+	cgroupPath := "/sys/fs/cgroup/minicontainer"
+	os.MkdirAll(cgroupPath, 0755)
+
+	must(os.WriteFile(
+		cgroupPath+"/pids.max",
+		[]byte("100"),
+		0700,
+	))
+
+	must(os.WriteFile(
+		cgroupPath+"/cpu.max",
+		[]byte("50000 100000"),
+		0700,
+	))
+
+	must(os.WriteFile(cgroupPath+"/memory.max", []byte("100000000"), 0700))
+
+	must(os.WriteFile(cgroupPath+"/cgroup.procs", []byte(fmt.Sprintf("%d", os.Getpid())), 0700))
+}
+
 func child() {
+
+	cg()
+
+	must(syscall.Mount("", "/", "", syscall.MS_PRIVATE|syscall.MS_REC, ""))
+
+	must(exec.Command("ip", "link", "set", "lo", "up").Run())
 	must(syscall.Sethostname([]byte("container")))
 
 	must(syscall.Chroot("./rootfs"))
